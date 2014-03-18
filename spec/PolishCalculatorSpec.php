978b8673257af60e25312b4c32c94a27c9df1be4
Alberto Ramirez <alberto@aramirez.es>
2014-03-18 21:32:53 +0100

    Added test checking that sum three numbers

--- spec/PolishCalculatorSpec.php
+++ spec/PolishCalculatorSpec.php
@@ -11,5 +11,12 @@ class PolishCalculatorSpec extends ObjectBehavior
 	{
 		$this->calculate("11+")->shouldReturn(2);
 		$this->calculate("12+")->shouldReturn(3);
+		$this->calculate("53+")->shouldReturn(8);
+	}
+
+	public function it_should_sum_three_numbers()
+	{
+		$this->calculate("123+")->shouldReturn(6);
+		$this->calculate("417+")->shouldReturn(12);
 	}
 }
--- src/PolishCalculator.php
+++ src/PolishCalculator.php
@@ -5,6 +5,9 @@ class PolishCalculator
 
     public function calculate($operation)
     {
+    	if (is_int((int)$operation{2})) {
+    		return (int) $operation{0} + (int) $operation{1} + (int) $operation{2};
+    	}
     	return (int) $operation{0} + (int) $operation{1};
     }
 }

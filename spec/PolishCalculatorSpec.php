b79ad194056e0be7db8bd7a4c0979b921fa26d7a
Alberto Ramirez <alberto@aramirez.es>
2014-03-21 16:25:04 +0100

    right operands

--- spec/PolishCalculatorSpec.php
+++ spec/PolishCalculatorSpec.php
@@ -16,13 +16,13 @@ class PolishCalculatorSpec extends ObjectBehavior
 
 	public function it_should_sum_three_numbers()
 	{
-		$this->calculate("123+")->shouldReturn(6);
-		$this->calculate("417+")->shouldReturn(12);
+		$this->calculate("123++")->shouldReturn(6);
+		$this->calculate("417++")->shouldReturn(12);
 	}
 
 	public function it_should_sum_as_numbers_as_given()
 	{
-		$this->calculate("123456789+")->shouldReturn(45);
-		$this->calculate("34291+")->shouldReturn(19);
+		$this->calculate("123456789++++++++")->shouldReturn(45);
+		$this->calculate("34291++++")->shouldReturn(19);
 	}
 }
